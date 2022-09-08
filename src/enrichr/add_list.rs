cdad92c77e9a9825b402c63135d3386012d17cb4
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-08 15:43:46 -0700

    added serialize and a display for each of the structs

--- src/enrichr/add_list.rs
+++ src/enrichr/add_list.rs
@@ -1,14 +1,20 @@
 use reqwest::{Client, Error};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::fmt;
 
 /// A struct to handle the results of the `Enrichr` function call: `addList`
 ///
 /// details at: <https://maayanlab.cloud/Enrichr/help#api&q=1>
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all="camelCase")]
 pub struct ResponseAddList {
     pub user_list_id: usize,
     pub short_id: String
+}
+impl fmt::Display for ResponseAddList {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", serde_json::to_string_pretty(&self).expect("cannot serialize"))
+    }
 }
 
 /// Performs a function call to the `addList` API.
