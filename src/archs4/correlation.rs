4c224af79e276fcc7132d02ede1a7666a3fa2378
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-09 16:06:09 -0700

    added testing for known gene AP2S1

--- src/archs4/correlation.rs
+++ src/archs4/correlation.rs
@@ -77,6 +77,24 @@ pub fn correlation(gene_name: &str, count: usize) -> Result<Correlations> {
 fn build_query(gene_name: &str, count: usize) -> HashMap<String, String> {
     let mut map = HashMap::new();
     map.insert(String::from("id"), gene_name.to_string());
-    map.insert(String::from("count"), format!("{}", count+1));
+    map.insert(String::from("count"), format!("{}", count));
     map
+}
+
+#[cfg(test)]
+mod testing {
+    use super::correlation;
+
+    #[test]
+    fn test_known() {
+        let symbol = "AP2S1";
+        let count = 5;
+        let results = correlation(symbol, count).unwrap();
+        assert_eq!(results.correlations.len(), count);
+        assert_eq!(results.correlations[0].gene_symbol, "AP2S1");
+        assert_eq!(results.correlations[1].gene_symbol, "ASNA1");
+        assert_eq!(results.correlations[2].gene_symbol, "MRPL28");
+        assert_eq!(results.correlations[3].gene_symbol, "SSNA1");
+        assert_eq!(results.correlations[4].gene_symbol, "COX8A");
+    }
 }
