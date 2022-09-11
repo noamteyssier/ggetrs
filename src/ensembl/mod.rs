dd824294572cd05a6dc125a781127284310f4767
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-11 11:59:18 -0700

    publically show python enesembl search

--- src/ensembl/mod.rs
+++ src/ensembl/mod.rs
@@ -6,5 +6,9 @@ mod search;
 /// submodule for command line usage
 mod submodule;
 
+/// submodule for python usage
+mod python;
+
 pub use search::search;
 pub use submodule::launch_ensembl_search;
+pub use python::python_ensembl_search;
