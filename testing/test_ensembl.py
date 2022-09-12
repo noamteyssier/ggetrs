bbedabd513acdba92ddce81c3f81db3c4c2c9451
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-11 21:28:59 -0700

    added a default value for ensembl search for python API

--- testing/test_ensembl.py
+++ testing/test_ensembl.py
@@ -1,24 +1,16 @@
 import ggetrs
 
 def test_search_toplevel():
-    species = "homo_sapiens"
-    db_type = "core"
-    release = 107
-    assembly = "38"
     search_terms = ["AP2S1"]
-    results = ggetrs.search(search_terms, species, db_type, release, assembly)
+    results = ggetrs.search(search_terms)
     assert("results" in results.keys())
     assert(len(results["results"]) == 1)
     assert("display_label" in results["results"][0].keys())
     assert(results["results"][0]["display_label"] == "AP2S1")
 
 def test_search_module():
-    species = "homo_sapiens"
-    db_type = "core"
-    release = 107
-    assembly = "38"
     search_terms = ["AP2S1"]
-    results = ggetrs.ensembl.search(search_terms, species, db_type, release, assembly)
+    results = ggetrs.ensembl.search(search_terms)
     assert("results" in results.keys())
     assert(len(results["results"]) == 1)
     assert("display_label" in results["results"][0].keys())
