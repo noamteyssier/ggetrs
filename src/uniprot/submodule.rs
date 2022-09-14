de93f76c4496d3733344d64a4d917f752de6d451
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-14 13:11:20 -0700

    added taxon identifier as option for filtering queries

--- src/uniprot/submodule.rs
+++ src/uniprot/submodule.rs
@@ -2,8 +2,8 @@ use anyhow::Result;
 use std::{io::Write, fs::File};
 use super::query;
 
-pub fn launch_uniprot_query(search_terms: &Vec<String>, output: &Option<String>) -> Result<()> {
-    let results = query(search_terms)?;
+pub fn launch_uniprot_query(search_terms: &Vec<String>, taxon: &Option<usize>, output: &Option<String>) -> Result<()> {
+    let results = query(search_terms, taxon)?;
     match output {
         Some(path) => {
             if let Ok(mut writer) = File::create(path) {
