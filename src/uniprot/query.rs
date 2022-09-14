279f770dfad970f998b11a5484b818d8883ad4b7
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-14 13:11:38 -0700

    split query builder into separate function to make it easier to add future filters

--- src/uniprot/query.rs
+++ src/uniprot/query.rs
@@ -3,13 +3,24 @@ use reqwest::{Result, Client};
 use serde_json::Value;
 use super::{UniprotInfo, UniprotInfoContainer};
 
-/// An asynchronous function which performs a uniprot query
-async fn async_query_uniprot(gene: &str) -> Result<Option<UniprotInfo>> {
-    let query = if gene.starts_with("ENS") {
+fn build_query_string(gene: &str, taxon: &Option<usize>) -> String {
+    let gene_query = if gene.starts_with("ENS") {
         gene.to_string()
     } else {
         format!("(gene:{})", gene)
     };
+    let taxon_query = match taxon {
+        Some(t) => {
+            format!("AND(taxonomy_id:{})", t)
+        },
+        None => String::new()
+    };
+    format!("{}{}", gene_query, taxon_query)
+}
+
+/// An asynchronous function which performs a uniprot query
+async fn async_query_uniprot(gene: &str, taxon: &Option<usize>) -> Result<Option<UniprotInfo>> {
+    let query = build_query_string(gene, taxon);
     let url = format!(
         "https://rest.uniprot.org/uniprotkb/search?query={}+AND+reviewed:true",
         query 
@@ -25,24 +36,25 @@ async fn async_query_uniprot(gene: &str) -> Result<Option<UniprotInfo>> {
 }
 
 /// An asynchronous function which joins all the handles from `async_query_uniprot`
-async fn async_query_uniprot_multiple(ensembl_ids: &Vec<String>) -> Result<Vec<Result<Option<UniprotInfo>>>> {
+async fn async_query_uniprot_multiple(ensembl_ids: &Vec<String>, taxon: &Option<usize>) -> Result<Vec<Result<Option<UniprotInfo>>>> {
     let query_handles = ensembl_ids
         .iter()
-        .map(|x| async_query_uniprot(x));
+        .map(|x| async_query_uniprot(x, taxon));
 
     let results = join_all(query_handles).await;
     Ok(results)
 }
 
 /// A synchronous function to perform a query for each of the terms provided.
-pub fn query(terms: &Vec<String>) -> anyhow::Result<UniprotInfoContainer> {
+pub fn query(terms: &Vec<String>, taxon: &Option<usize>) -> anyhow::Result<UniprotInfoContainer> {
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
+
     let results = rt.block_on(async move {
-        async_query_uniprot_multiple(terms).await.expect("could not query uniprot")
-    });
-    let results = results
+            async_query_uniprot_multiple(terms, taxon)
+                .await.expect("could not query uniprot")
+        })
         .into_iter()
         .filter_map(|x| x.expect("could not create results"))
         .collect();
