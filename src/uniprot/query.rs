1a80bb27d43a396e152a51db7414d2550cadbba2
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-14 12:30:40 -0700

    create uniprotinfo struct as option and filtermap to avoid crashing on null requests

--- src/uniprot/query.rs
+++ src/uniprot/query.rs
@@ -31,24 +31,17 @@ impl fmt::Display for UniprotInfo {
     }
 }
 impl UniprotInfo {
-    pub fn from_value(value: Value, query: &str) -> Self {
+    pub fn from_value(value: Value, query: &str) -> Option<Self> {
+        if !Self::is_valid(&value) { return None }
         let uniprot_id = Self::get_uniprot_id(&value);
-        // println!("{:?}", uniprot_id);
         let primary_gene_name = Self::get_primary_gene_name(&value);
-        // println!("{:?}", primary_gene_name);
         let uniprot_synonyms = Self::get_uniprot_synonyms(&value);
-        // println!("{:?}", uniprot_synonyms);
         let protein_name = Self::get_protein_names(&value);
-        // println!("{:?}", protein_name);
         let uniprot_description = Self::get_uniprot_description(&value);
-        // println!("{:?}", uniprot_description);
         let ncbi_id = Self::get_ncbi_id(&value);
-        // println!("{:?}", ncbi_id);
         let pdb_id = Self::get_pdb_id(&value);
-        // println!("{:?}", pdb_id);
         let query = query.to_string();
-        // println!("{:?}", ensembl_id);
-        Self {
+        Some(Self {
             uniprot_id,
             primary_gene_name,
             uniprot_synonyms,
@@ -57,7 +50,11 @@ impl UniprotInfo {
             ncbi_id,
             pdb_id,
             query 
-        }
+        })
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        !value["results"][0].is_null()
     }
 
     fn get_uniprot_id(value: &Value) -> String {
@@ -133,7 +130,7 @@ impl UniprotInfo {
     }
 }
 
-async fn async_query_uniprot(gene: &str) -> reqwest::Result<UniprotInfo> {
+async fn async_query_uniprot(gene: &str) -> reqwest::Result<Option<UniprotInfo>> {
     let query = if gene.starts_with("ENS") {
         gene.to_string()
     } else {
@@ -153,7 +150,7 @@ async fn async_query_uniprot(gene: &str) -> reqwest::Result<UniprotInfo> {
         .map(|x| UniprotInfo::from_value(x, gene))
 }
 
-async fn async_query_uniprot_multiple(ensembl_ids: &Vec<String>) -> reqwest::Result<Vec<reqwest::Result<UniprotInfo>>> {
+async fn async_query_uniprot_multiple(ensembl_ids: &Vec<String>) -> reqwest::Result<Vec<reqwest::Result<Option<UniprotInfo>>>> {
     let query_handles = ensembl_ids
         .iter()
         .map(|x| async_query_uniprot(x));
@@ -169,7 +166,10 @@ pub fn query(terms: &Vec<String>) -> anyhow::Result<UniprotInfoContainer> {
     let results = rt.block_on(async move {
         async_query_uniprot_multiple(terms).await.expect("could not query uniprot")
     });
-    let results = results.into_iter().map(|x| x.expect("could not create results")).collect();
+    let results = results
+        .into_iter()
+        .filter_map(|x| x.expect("could not create results"))
+        .collect();
     Ok(UniprotInfoContainer(results))
 
 }
