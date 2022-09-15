f78bb4618601a4c98b9aa671d471868cb42090db
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-14 22:31:46 -0700

    converted archs4 correlation into new directory structure

--- src/archs4/mod.rs
+++ src/archs4/mod.rs
@@ -1,5 +1,5 @@
-/// module for querying most correlated genes from a query gene
-mod correlation;
+mod functions;
+mod types;
 
 /// module for querying the tissues where a query gene is expressed
 mod tissue;
@@ -10,7 +10,7 @@ mod submodule;
 /// module for python interface
 mod python;
 
-pub use correlation::correlation;
+pub use functions::correlation;
 pub use python::python_archs4;
 pub use submodule::{launch_archs4_correlation, launch_archs4_tissue};
 pub use tissue::{tissue, Species};
--- src/archs4/types/correlation.rs
+++ src/archs4/types/correlation.rs
@@ -1,7 +1,5 @@
 use pyo3::{types::PyDict, PyResult, Python};
-use reqwest::{blocking::Client, Result};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::fmt;
 
 /// Deserialization of the ARCHS4 result json
@@ -95,43 +93,3 @@ impl Correlation {
     }
 }
 
-/// Queries the most correlated genes for a provided gene.
-///
-/// Uses the ARCHS4 correlation API
-pub fn correlation(gene_name: &str, count: usize) -> Result<Correlations> {
-    let client = Client::new();
-    let url = "https://maayanlab.cloud/matrixapi/coltop";
-    let map = build_query(gene_name, count);
-    let correlations = client
-        .post(url)
-        .json(&map)
-        .send()?
-        .json::<ResultCorrelation>()?;
-    Ok(correlations.into())
-}
-
-/// Builds the `HashMap` to be converted to a JSON
-fn build_query(gene_name: &str, count: usize) -> HashMap<String, String> {
-    let mut map = HashMap::new();
-    map.insert(String::from("id"), gene_name.to_string());
-    map.insert(String::from("count"), format!("{}", count));
-    map
-}
-
-#[cfg(test)]
-mod testing {
-    use super::correlation;
-
-    #[test]
-    fn test_known() {
-        let symbol = "AP2S1";
-        let count = 5;
-        let results = correlation(symbol, count).unwrap();
-        assert_eq!(results.correlations.len(), count);
-        assert_eq!(results.correlations[0].gene_symbol, "AP2S1");
-        assert_eq!(results.correlations[1].gene_symbol, "ASNA1");
-        assert_eq!(results.correlations[2].gene_symbol, "MRPL28");
-        assert_eq!(results.correlations[3].gene_symbol, "SSNA1");
-        assert_eq!(results.correlations[4].gene_symbol, "COX8A");
-    }
-}
