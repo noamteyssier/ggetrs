63e4f66b1ce8b5694b499a7d8e88747cfbab1f7b
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-14 17:44:37 -0700

    split up formats for query url creation

--- src/archs4/tissue.rs
+++ src/archs4/tissue.rs
@@ -134,11 +134,12 @@ impl ResultTissue {
 /// Returns the tissue-specific expression of a provided `gene_name`
 pub fn tissue(gene_name: &str, species: &Species) -> Result<ResponseTissue> {
     let client = Client::new();
+    let query_string = format!("search={}&species={}&type=tissue", gene_name, species);
+
 
     let url = format!(
         "https://maayanlab.cloud/archs4/search/loadExpressionTissue.php?{}",
-        format!("search={}&species={}&type=tissue", gene_name, species)
-        );
+        query_string);
 
     let raw_response = client.post(url)
         .header("Content-Type", "application/json")
