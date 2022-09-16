caf7b7367e425ba0ef974c8e371ebadd7bccd4b4
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-16 14:17:09 -0700

    reordered results from ensembl lookup to be more standard (#74)
    * reordered results from ensembl lookup to be more standard
    
    * ran cargo fmt

--- src/utils/parsing.rs
+++ src/utils/parsing.rs
@@ -47,14 +47,19 @@ pub fn parse_secondary_vec_string(value: &Value, primary: &str, secondary: &str)
 
 /// Parses an optional vec from a json object from a secondary level
 #[must_use]
-pub fn parse_secondary_vec_optional_string(value: &Value, primary: &str, secondary: &str) -> Option<Vec<String>> {
+pub fn parse_secondary_vec_optional_string(
+    value: &Value,
+    primary: &str,
+    secondary: &str,
+) -> Option<Vec<String>> {
     match value[primary][secondary].as_array() {
         Some(arr) => {
-            let vec = arr.iter()
+            let vec = arr
+                .iter()
                 .map(|x| x.as_str().expect("Non-string found in array").to_string())
                 .collect();
             Some(vec)
-        },
-        None => None
+        }
+        None => None,
     }
 }
