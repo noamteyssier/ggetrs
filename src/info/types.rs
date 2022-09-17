96abc33e9eccaa1e270aaf6a301d053cee90bf02
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-16 18:17:22 -0700

    started info results types and will add to it in future PRs

--- src/info/types.rs
+++ src/info/types.rs
@@ -1,7 +1,19 @@
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
 use crate::{ensembl::types::LookupResponse, uniprot::UniprotInfoContainer, ncbi::types::NcbiResults};
+use std::fmt;
 
-pub struct InfoContainer {
-    results: Vec<Info>
+/// Container for `Info` which aggregates results from multiple databases
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InfoContainer (pub HashMap<String, Info>);
+impl fmt::Display for InfoContainer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_json::to_string_pretty(&self).expect("cannot serialize")
+        )
+    }
 }
 impl InfoContainer {
     pub fn from_queries(
@@ -9,19 +21,85 @@ impl InfoContainer {
         uniprot: &UniprotInfoContainer, 
         ncbi: &NcbiResults) -> Self 
     {
-        let results = Vec::new();
-        Self { results }
+        let map = ncbi
+            .0
+            .keys()
+            .filter_map(|k| {
+                match Info::from_queries(ensembl, uniprot, ncbi, k) {
+                    Some(info) => Some((k.to_string(), info)),
+                    None => None
+                }
+            })
+            .collect();
+        Self(map)
     }
 
 }
 
-pub struct Info;
+/// Container which aggregates query results from multiple databases
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Info {
+    ensembl_id: String,
+    uniprot_id: String,
+    ncbi_id: String,
+    symbol: String,
+    ensembl_description: String,
+    uniprot_description: String,
+    ncbi_description: String,
+    species: String,
+    assembly_name: String,
+}
+impl fmt::Display for Info {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_json::to_string_pretty(&self).expect("cannot serialize")
+        )
+    }
+}
 impl Info {
     pub fn from_queries(
         ensembl: &LookupResponse, 
         uniprot: &UniprotInfoContainer, 
-        ncbi: &NcbiResults) -> Self 
+        ncbi: &NcbiResults,
+        key: &str) -> Option<Self>
     {
-        Self
+        let ensembl_result = match ensembl.0.get(key) {
+            Some(opt_result) => match opt_result {
+                Some(result) => result,
+                None => return None
+            },
+            None => return None
+        };
+        let uniprot_result = match uniprot.0.get(key) {
+            Some(result) => result,
+            None => return None
+        };
+        let ncbi_result = match ncbi.0.get(key) {
+            Some(result) => result,
+            None => return None
+        };
+        let ensembl_id = ensembl_result.id.to_owned();
+        let uniprot_id = uniprot_result.uniprot_id.to_owned();
+        let ncbi_id = ncbi_result.gene_id.to_owned();
+        let symbol = ncbi_result.symbol.to_owned();
+        let ensembl_description = ensembl_result.description.to_owned();
+        let uniprot_description = uniprot_result.uniprot_description.to_owned();
+        let ncbi_description = ncbi_result.description.to_owned();
+        let species = ensembl_result.species.to_owned();
+        let assembly_name = ensembl_result.assembly_name.to_owned();
+
+        Some(Self {
+            ensembl_id,
+            uniprot_id,
+            ncbi_id,
+            symbol,
+            ensembl_description,
+            uniprot_description,
+            ncbi_description,
+            species,
+            assembly_name,
+        })
     }
 }
