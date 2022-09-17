6fbd47376681fa77042df4f68e5fe455dce9a9ad
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-17 15:33:37 -0700

    added a minimum sequence size to ucsc blat

--- src/ucsc/functions/blat.rs
+++ src/ucsc/functions/blat.rs
@@ -1,10 +1,13 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use reqwest::blocking::Client;
 use serde_json::Value;
 use crate::ucsc::types::{SeqType, BlatResults};
 
 /// Queries an input sequence for location within the genome
 pub fn blat(sequence: &str, seqtype: &SeqType, db_name: &str) -> Result<BlatResults> {
+    if sequence.len() < 20 {
+        bail!("Input sequence must be greater than 20 nucleotides/residues")
+    }
     let url = format!(
         "https://genome.ucsc.edu/cgi-bin/hgBlat?userSeq={}&type={}&db={}&output=json",
         sequence,
