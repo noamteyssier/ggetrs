96a74b2606a994afb39e3ca592f82f0436ac53ab
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-16 18:17:44 -0700

    added output options for cli info

--- src/info/submodule.rs
+++ src/info/submodule.rs
@@ -1,7 +1,21 @@
 use anyhow::Result;
 use super::info;
+use std::{io::Write, fs::File};
 
-pub fn launch_info(search_terms: &[String], species: &str, taxon_id: usize) -> Result<()> {
-    info(search_terms, species, taxon_id)?;
+/// Main entrypoint for info query across multiple databases
+pub fn launch_info(search_terms: &[String], species: &str, taxon_id: usize, output: &Option<String>) -> Result<()> {
+    let results = info(search_terms, species, taxon_id)?;
+    match output {
+        Some(path) => {
+            if let Ok(mut writer) = File::create(path) {
+                writeln!(writer, "{}", results).expect("Unable to write to file");
+            } else {
+                println!("{}", results);
+            }
+        }
+        None => {
+            println!("{}", results);
+        }
+    }
     Ok(())
 }
