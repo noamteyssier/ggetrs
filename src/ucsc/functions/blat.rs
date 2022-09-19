7f1c6ca59aa65ee72607adcbf790f7e8fd0093a1
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-18 18:30:43 -0700

    108 testing for ucsc (#109)
    * made attributes of type public
    
    * added testing for ucsc blat
    
    * bump version
    
    * cargo fmt

--- src/ucsc/functions/blat.rs
+++ src/ucsc/functions/blat.rs
@@ -16,3 +16,57 @@ pub fn blat(sequence: &str, seqtype: &SeqType, db_name: &str) -> Result<BlatResu
     let br = BlatResults::from_value(&response);
     Ok(br)
 }
+
+#[cfg(test)]
+mod testing {
+    use super::blat;
+    use crate::ucsc::types::SeqType;
+
+    #[test]
+    fn test_blat_short_seq() {
+        let sequence = "ACTG";
+        let seqtype = SeqType::Dna;
+        let db_name = "hg38";
+        let response = blat(sequence, &seqtype, db_name);
+        assert!(response.is_err());
+    }
+
+    #[test]
+    fn test_blat() {
+        let sequence = "AGTGGTACATGCAGTTTGATGATGATGAGAAACAGAAGCT";
+        let seqtype = SeqType::Dna;
+        let db_name = "hg38";
+        let response = blat(sequence, &seqtype, db_name).unwrap();
+        assert_eq!(response.0[0].matches, 40);
+        assert_eq!(response.0.len(), 3);
+    }
+
+    #[test]
+    fn test_blat_protein() {
+        let sequence = "MIRFILIQNRAGKTRLAKWYMQFDDDEKQK";
+        let seqtype = SeqType::Protein;
+        let db_name = "hg38";
+        let response = blat(sequence, &seqtype, db_name).unwrap();
+        assert_eq!(response.0[0].matches, 29);
+        assert_eq!(response.0.len(), 1);
+    }
+
+    #[test]
+    fn test_blat_missing_db() {
+        let sequence = "AGTGGTACATGCAGTTTGATGATGATGAGAAACAGAAGCT";
+        let seqtype = SeqType::Dna;
+        let db_name = "blahblahblah";
+        let response = blat(sequence, &seqtype, db_name).unwrap();
+        assert_eq!(response.0[0].matches, 40);
+        assert_eq!(response.0.len(), 3);
+    }
+
+    #[test]
+    fn test_blat_wrong_seqtype() {
+        let sequence = "MIRFILIQNRAGKTRLAKWYMQFDDDEKQK";
+        let seqtype = SeqType::Dna;
+        let db_name = "hg38";
+        let response = blat(sequence, &seqtype, db_name);
+        assert!(response.is_err())
+    }
+}
