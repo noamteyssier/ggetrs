877c4831d6774c3baee59d07dfce6efe532181ec
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-18 19:51:28 -0700

    added testing for taxon function in ncbi

--- src/ncbi/functions/taxons.rs
+++ src/ncbi/functions/taxons.rs
@@ -1,18 +1,21 @@
+use anyhow::{Result, bail};
 use reqwest::blocking::Client;
 use serde_json::Value;
-
 use crate::ncbi::types::{Taxon, TaxonContainer};
 
-pub fn taxons(query: &str, limit: usize) -> anyhow::Result<TaxonContainer> {
+/// Finds taxon information from a string query
+pub fn taxons(query: &str, limit: usize) -> Result<TaxonContainer> {
     let url = format!(
         "https://api.ncbi.nlm.nih.gov/datasets/v1/gene/taxon_suggest/{}",
         query
     );
+
     let response = Client::new()
         .get(url)
         .header("accept", "application/json")
         .send()?
         .json::<Value>()?;
+
     let results = response["sci_name_and_ids"]
         .as_array()
         .map(|array| {
@@ -23,5 +26,32 @@ pub fn taxons(query: &str, limit: usize) -> anyhow::Result<TaxonContainer> {
                 .collect::<Vec<Taxon>>()
         })
         .unwrap_or(Vec::new());
-    Ok(TaxonContainer(results))
+
+    if results.len() == 0 {
+        bail!(format!("No results found for query: {}", query))
+    } else {
+        Ok(TaxonContainer(results))
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    use super::taxons;
+
+    #[test]
+    fn test_query() {
+        let query = "human";
+        let limit = 1;
+        let response = taxons(query, limit).unwrap();
+        assert_eq!(response.0.len(), 1);
+        assert_eq!(response.0[0].tax_id, "9606");
+    }
+
+    #[test]
+    fn test_query_nonsense() {
+        let query = "asjdiawjdpansopdnawd";
+        let limit = 1;
+        let response = taxons(query, limit);
+        assert!(response.is_err());
+    }
 }
