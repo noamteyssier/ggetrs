a4737e3796294582e91cad21de05c887cea8e745
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-18 19:30:06 -0700

    107 testing for pdb (#110)
    * added testing on pdb info
    
    * added testing for resource type pdb
    
    * added testing for pdb structure
    
    * added testing for structure pdb
    
    * bump version
    
    * cargo fmt

--- src/pdb/functions/resource.rs
+++ src/pdb/functions/resource.rs
@@ -40,5 +40,70 @@ pub fn resource_info(
         format!("{}/{}/{}", base_url, resource, pdb_id)
     };
     let response = Client::new().get(rest_url).send()?.json::<Value>()?;
-    Ok(response)
+    match response["status"].as_u64() {
+        Some(code) => bail!(format!("Error: HTTP Code {}", code)),
+        None => Ok(response),
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    use super::resource_info;
+    use crate::pdb::types::PdbResource;
+
+    #[test]
+    fn test_resource_entry() {
+        let pdb_id = "6URI"; // AP2S1
+        let resource = PdbResource::Entry;
+        let identifier = None;
+        let info = resource_info(pdb_id, &resource, &identifier).unwrap();
+        assert_eq!(info["entry"]["id"].as_str(), Some(pdb_id));
+    }
+
+    #[test]
+    fn test_resource_pubmed() {
+        let pdb_id = "6URI"; // AP2S1
+        let resource = PdbResource::Pubmed;
+        let identifier = None;
+        let info = resource_info(pdb_id, &resource, &identifier).unwrap();
+        assert_eq!(info["rcsb_id"].as_str(), Some("32719457"));
+    }
+
+    #[test]
+    fn test_resource_assembly() {
+        let pdb_id = "6URI"; // AP2S1
+        let resource = PdbResource::Assembly;
+        let identifier = Some(String::from("1"));
+        let info = resource_info(pdb_id, &resource, &identifier).unwrap();
+        assert_eq!(info["rcsb_id"].as_str(), Some("6URI-1"));
+    }
+
+    #[test]
+    fn test_resource_missing_id() {
+        let pdb_id = "MISSINGID";
+        let resource = PdbResource::Entry;
+        let identifier = None;
+        let response = resource_info(pdb_id, &resource, &identifier);
+        assert!(response.is_err());
+    }
+
+    #[test]
+    fn test_resource_missing_identifier() {
+        let pdb_id = "6URI";
+        let identifier = None;
+        let resources = vec![
+            PdbResource::Assembly,
+            PdbResource::BranchedEntity,
+            PdbResource::NonpolymerEntity,
+            PdbResource::PolymerEntity,
+            PdbResource::Uniprot,
+            PdbResource::BranchedEntityInstance,
+            PdbResource::PolymerEntityInstance,
+            PdbResource::NonpolymerEntityInstance,
+        ];
+        for resource in resources {
+            let response = resource_info(pdb_id, &resource, &identifier);
+            assert!(response.is_err());
+        }
+    }
 }
