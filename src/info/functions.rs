44c6ffa313465ca928cf4cc846b7c8e2847b2095
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-18 20:23:39 -0700

    104 testing for info (#113)
    * added testing for info
    
    * bump version

--- src/info/functions.rs
+++ src/info/functions.rs
@@ -12,3 +12,35 @@ pub fn info(search_terms: &[String], species: &str, taxon_id: usize) -> Result<I
         &ncbi_query,
     ))
 }
+
+#[cfg(test)]
+mod testing {
+    use super::info;
+
+    #[test]
+    fn test_info() {
+        let search_terms = vec!["AP2S1".to_string()];
+        let species = "homo_sapiens";
+        let taxon_id = 9606;
+        let response = info(&search_terms, species, taxon_id);
+        assert!(response.is_ok())
+    }
+
+    #[test]
+    fn test_info_nonsense_query() {
+        let search_terms = vec!["ALSKDOAWKDASDN".to_string()];
+        let species = "homo_sapiens";
+        let taxon_id = 9606;
+        let response = info(&search_terms, species, taxon_id);
+        assert!(response.is_err())
+    }
+
+    #[test]
+    fn test_info_mismatch_species() {
+        let search_terms = vec!["AP2S1".to_string()];
+        let species = "homo_sapiens";
+        let taxon_id = 0000;
+        let response = info(&search_terms, species, taxon_id);
+        assert!(response.is_err())
+    }
+}
