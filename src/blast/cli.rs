86349307dbba5eb72210cba1c6ad14e1a9beba7e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-26 10:15:40 -0700

    added output write for blast

--- src/blast/cli.rs
+++ src/blast/cli.rs
@@ -1,8 +1,29 @@
 use anyhow::Result;
+use std::{io::Write, fs::File};
 use super::{types::{BlastProgram, BlastDatabase}, functions::blast};
 
-pub fn launch_blast(query: &str, program: &Option<BlastProgram>, database: &Option<BlastDatabase>, limit: usize, expect: f64, low_comp_filter: bool, megablast: bool) -> Result<()> {
-    let result = blast(query, program, database, limit, expect, low_comp_filter, megablast)?;
-    println!("{:#?}", result);
+pub fn launch_blast(
+    query: &str, 
+    program: &Option<BlastProgram>, 
+    database: &Option<BlastDatabase>, 
+    limit: usize, 
+    expect: f64, 
+    low_comp_filter: bool, 
+    megablast: bool,
+    output: &Option<String>,
+) -> Result<()> {
+    let results = blast(query, program, database, limit, expect, low_comp_filter, megablast)?;
+    match output {
+        Some(path) => {
+            if let Ok(mut writer) = File::create(path) {
+                writeln!(writer, "{}", results)?;
+            } else {
+                println!("{}", results);
+            }
+        }
+        None => {
+            println!("{}", results);
+        }
+    }
     Ok(())
 }
