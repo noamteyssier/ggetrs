3459a86ab9655a623288b3a512ab912d8a5c6c95
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-27 17:36:47 -0700

    made launch enrichr list public

--- src/enrichr/mod.rs
+++ src/enrichr/mod.rs
@@ -10,6 +10,6 @@ mod cli;
 /// submodule for python usage
 mod python;
 
-pub use cli::launch_enrich;
+pub use cli::{launch_enrichr, launch_enrichr_list};
 pub use functions::{add_list, enrich, get_libraries};
 pub use python::python_enrichr;
