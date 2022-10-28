d7da2c2f4a6a44ca793091b7a11a9e50c49b0ade
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-27 17:36:09 -0700

    moved enrichr to a subcommand

---
+++ src/cli/enrichr.rs
@@ -0,0 +1,42 @@
+use clap::Subcommand;
+
+#[derive(Subcommand)]
+pub enum ModEnrichr {
+
+    Enrichr {
+        /// any database listed at: https://maayanlab.cloud/Enrichr/#libraries
+        /// some shorthands include: pathway, transcription, ontology, diseases_drugs, celltypes,
+        /// and kinase_interactions.
+        #[clap(short, long)]
+        library: String,
+
+        /// list of gene symbols to perform enrichment analysis on.
+        #[clap(value_parser, required = true)]
+        gene_list: Vec<String>,
+
+        /// optional filepath to write output to [default=stdout]
+        #[clap(short, long)]
+        output: Option<String>,
+    },
+
+    /// List all available libraries and their descriptions
+    List {
+        
+        /// Return library names in plaintext
+        #[clap(short, long)]
+        minimal: bool,
+
+        /// List the categorization of libraries
+        #[clap(short = 't', long)]
+        list_categories: bool,
+
+        /// Filter to a category ID
+        #[clap(short, long)]
+        category: Option<usize>,
+
+        /// optional filepath to write output to [default=stdout]
+        #[clap(short, long)]
+        output: Option<String>,
+    }
+
+}
