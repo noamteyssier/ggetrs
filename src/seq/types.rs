8cbb47f3986f73e2aae297fd7a57a21bf2d09cc0
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-28 14:19:34 -0700

    use fastarecord and fastarecords to resultseq container and resultseq

--- src/seq/types.rs
+++ src/seq/types.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+use crate::utils::{FastaRecord, FastaRecords};
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResultSeqContainer(pub Vec<ResultSeq>);
-
 impl ResultSeqContainer {
     pub fn to_fasta(&self) -> String {
         self.0
@@ -10,17 +11,30 @@ impl ResultSeqContainer {
             .map(|x| x.to_fasta())
             .fold(String::new(), |acc, x| acc + &x)
     }
+    pub fn fasta_records(&self) -> FastaRecords {
+        let records = self.0
+            .iter()
+            .map(|x| x.as_fasta())
+            .collect();
+        FastaRecords(records)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResultSeq {
-    pub seq: String,
-    pub desc: String,
     pub id: String,
+    pub desc: String,
+    pub seq: String,
 }
 
 impl ResultSeq {
     pub fn to_fasta(&self) -> String {
-        format!(">{} {}\n{}\n", self.id, self.desc, self.seq)
+        format!("{}", self.as_fasta())
+    }
+    pub fn as_fasta(&self) -> FastaRecord {
+        FastaRecord::new(
+            &format!("{} {}", self.id, self.desc),
+            &self.seq
+        )
     }
 }
