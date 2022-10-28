73d71ddfb480c22a34a195f6a66850c93312eb8f
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-27 17:36:33 -0700

    added a launch list subcommand

--- src/enrichr/cli.rs
+++ src/enrichr/cli.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
 use std::io::Write;
 
-use super::{add_list, enrich};
+use super::{add_list, enrich, get_libraries};
 use reqwest::Result;
 
 /// Main entrypoint to launching the `enrich` function for `Enrichr`
-pub fn launch_enrich(library: &str, gene_list: &[String], output: &Option<String>) -> Result<()> {
+pub fn launch_enrichr(library: &str, gene_list: &[String], output: &Option<String>) -> Result<()> {
     let add_list = add_list(gene_list)?;
     let results = enrich(add_list.user_list_id, library)?;
     match output {
@@ -20,5 +20,40 @@ pub fn launch_enrich(library: &str, gene_list: &[String], output: &Option<String
             println!("{}", results);
         }
     }
+    Ok(())
+}
+
+/// Main entrypoint for listing all available libraries in `Enrichr`
+pub fn launch_enrichr_list(minimal: bool, list_categories: bool, category: &Option<usize>, output: &Option<String>) -> Result<()> {
+    let libraries = get_libraries()?;
+
+    let output_str = if list_categories {
+        format!("{}", libraries.categories())
+    } else {
+        let libs = if let Some(cid) = category {
+            libraries.filter_categories(*cid)
+        } else {
+            libraries.libraries()
+        };
+        if minimal {
+            format!("{}", libs.minimal())
+        } else {
+            format!("{}", libs)
+        }
+    };
+
+    match output {
+        Some(path) => {
+            if let Ok(mut writer) = File::create(path) {
+                writeln!(writer, "{}", output_str).expect("Unable to write to file");
+            } else {
+                println!("{}", output_str);
+            }
+        }
+        None => {
+            println!("{}", output_str);
+        }
+    }
+
     Ok(())
 }
