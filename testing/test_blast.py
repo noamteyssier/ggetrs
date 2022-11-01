b5f51142b4f56e2e06d82d33b5e273cf6168c246
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-11-01 10:48:31 -0700

    will add this back in once megablast is stable

--- testing/test_blast.py
+++
@@ -1,8 +0,0 @@
-import ggetrs
-
-def test_blast():
-    sequence = "ATACTCAGTCACACAAGCCATAGCAGGAAACAGCGAGCTTGCAGCCTCACCGACGAGTCTCAACTAAAAGGGACTCCCGGAGCTAGGGGTGGGGACTCGGCCTCACACAGTGAGTGCCGG"
-    result = ggetrs.blast(sequence, megablast = False)
-    assert result["query"] == sequence
-    assert len(result["results"]) >= 1
-    assert result["results"][0]["id"] == "gi|2310188890|ref|NG_029005.2|"
