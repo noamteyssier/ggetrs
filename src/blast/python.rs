3a701538f58de18d6eeb8ebb96c7c1ba026277b4
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-11-01 09:42:02 -0700

    api for python blast

---
+++ src/blast/python.rs
@@ -0,0 +1,43 @@
+use anyhow::{Result, bail};
+use clap::ValueEnum;
+use pyo3::{Python, types::{PyDict, IntoPyDict}, pyfunction};
+use super::{types::{BlastProgram, BlastDatabase}, functions::blast};
+
+#[pyfunction(name = "blast")]
+#[pyo3(text_signature = "(query, program = None, database = None, limit = 50, expect = 10.0, low_comp_filter = False, megablast = True)")]
+pub fn python_blast<'py>(
+    py: Python<'py>,
+    query: &str,
+    program: Option<String>,
+    database: Option<String>,
+    limit: Option<usize>,
+    expect: Option<f64>,
+    low_comp_filter: Option<bool>,
+    megablast: Option<bool>,
+) -> Result<&'py PyDict> {
+
+    let program = match program {
+        Some(program_str) => if let Ok(s) = BlastProgram::from_str(&program_str, true) {
+            Some(s)
+        } else {
+            bail!("Could not assign blast program from input")
+        },
+        None => None
+    };
+
+    let database = match database {
+        Some(database_str) => if let Ok(s) = BlastDatabase::from_str(&database_str, true) {
+            Some(s)
+        } else {
+            bail!("Could not assign blast database from input")
+        },
+        None => None
+    };
+
+    let limit = limit.unwrap_or(50);
+    let expect = expect.unwrap_or(10.0);
+    let low_comp_filter = low_comp_filter.unwrap_or(false);
+    let megablast = megablast.unwrap_or(true);
+    let response = blast(query, &program, &database, limit, expect, low_comp_filter, megablast)?;
+    Ok(response.into_py_dict(py))
+}
