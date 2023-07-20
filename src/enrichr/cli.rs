6378fdd0204668967bb7168a7a166e52c236921c
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-20 14:14:06 -0700

    two entrypoints for enrichr with background

--- src/enrichr/cli.rs
+++ src/enrichr/cli.rs
@@ -1,13 +1,28 @@
 use std::fs::File;
 use std::io::Write;
 
-use super::{add_list, enrich, get_libraries};
-use reqwest::Result;
+use super::{add_list, enrich, get_libraries, functions::add_background};
+use anyhow::{bail, Result};
 
 /// Main entrypoint to launching the `enrich` function for `Enrichr`
-pub fn launch_enrichr(library: &str, gene_list: &[String], output: &Option<String>) -> Result<()> {
-    let add_list = add_list(gene_list)?;
-    let results = enrich(add_list.user_list_id, library)?;
+pub fn launch_enrichr(library: &str, background: &Option<Vec<String>>, gene_list: &[String], output: &Option<String>) -> Result<()> {
+
+    // entrypoint for enrichr with background
+    let results = if let Some(background_set) = background {
+        let background_id = add_background(background_set)?;
+        let add_list = add_list(gene_list, true)?;
+        let missing_genes = gene_list.iter().filter(|x| !background_set.contains(x)).collect::<Vec<&String>>();
+        if !missing_genes.is_empty() {
+            eprintln!("The following genes were not found in the background set: {:?}", missing_genes);
+            bail!("Unable to find all genes in background set");
+        }
+        enrich(add_list.user_list_id, library, Some(&background_id.backgroundid))?
+    // standard entrypoint for enrichr
+    } else {
+        let add_list = add_list(gene_list, false)?;
+        enrich(add_list.user_list_id, library, None)?
+    };
+
     match output {
         Some(path) => {
             if let Ok(mut writer) = File::create(path) {
