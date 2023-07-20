506ea299fd370ba3a8f5f1544524c1dd092f1336
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-20 14:17:46 -0700

    use backgroundenrichr function entrypoint and parse from string to fix Infinity representation in json

--- src/enrichr/functions/enrich.rs
+++ src/enrichr/functions/enrich.rs
@@ -1,40 +1,94 @@
 use crate::enrichr::types::ResponseEnrich;
-use reqwest::Result;
-
-use super::shorthand;
+use reqwest::blocking::Client;
+use super::{shorthand, ENRICHR_URL, SPEEDRICHR_URL};
+use anyhow::Result;
 
 /// Performs an API call to the `Enrichr`'s `enrich`.
 ///
 /// This measures the significance of overlap of the provided gene list to the provided library
 /// name.
-pub fn enrich(list_id: usize, library_name: &str) -> Result<ResponseEnrich> {
+pub fn enrich(
+    list_id: usize, 
+    library_name: &str,
+    background_id: Option<&str>,
+) -> Result<ResponseEnrich> {
     let alias = shorthand(library_name);
-    let url = format!(
-        "https://maayanlab.cloud/Enrichr/enrich?userListId={}&backgroundType={}",
-        list_id, alias
-    );
-    reqwest::blocking::get(url)?.json::<ResponseEnrich>()
+    let url = if background_id.is_some() {
+        format!("{}/api/backgroundenrich", SPEEDRICHR_URL)
+    } else {
+        format!("{}/enrich", ENRICHR_URL)
+    };
+
+    if let Some(background_id) = background_id {
+        let form = reqwest::blocking::multipart::Form::new()
+            .text("userListId", list_id.to_string())
+            .text("backgroundid", background_id.to_string())
+            .text("backgroundType", alias.to_string());
+        let client = Client::new();
+        // Go through intermediate `text` to replace `Infinity` with `f64::MIN_POSITIVE`.
+        let text = client
+            .post(url.clone())
+            .multipart(form)
+            .send()?
+            .text()?
+            .replace("Infinity", format!("{:e}", f64::MIN_POSITIVE).as_str());
+        // Parse the JSON from a string.
+        Ok(serde_json::from_str::<ResponseEnrich>(&text)?)
+    } else {
+        let request_url = format!(
+            "{}/enrich?userListId={}&backgroundType={}",
+            ENRICHR_URL, list_id, alias
+        );
+        Ok(reqwest::blocking::get(request_url)?.json::<ResponseEnrich>()?)
+    }
 }
 
 #[cfg(test)]
 mod testing {
     use super::enrich;
-    use crate::enrichr::add_list;
+    use crate::enrichr::functions::{add_list, add_background};
 
     fn get_list_id() -> usize {
         let gene_list = vec!["AP2S1", "NSD1", "LDB1"]
             .iter()
             .map(|x| x.to_string())
             .collect::<Vec<String>>();
-        let response = add_list(&gene_list).unwrap();
+        let response = add_list(&gene_list, false).unwrap();
         response.user_list_id
+    }
+
+    fn get_list_id_with_background() -> usize {
+        let gene_list = vec!["AP2S1", "NSD1", "LDB1"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        let response = add_list(&gene_list, true).unwrap();
+        response.user_list_id
+    }
+
+    fn get_background_id() -> String {
+        let gene_list = vec!["AP2S1", "NSD1", "LDB1", "RFX3"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        let response = add_background(&gene_list).unwrap();
+        response.backgroundid
     }
 
     #[test]
     fn test_enrich() {
         let user_list_id = get_list_id();
         let library_name = "KEGG_2015";
-        let response = enrich(user_list_id, library_name).unwrap();
+        let response = enrich(user_list_id, library_name, None).unwrap();
+        assert!(response.0.contains_key(library_name));
+    }
+
+    #[test]
+    fn test_enrich_with_background() {
+        let user_list_id = get_list_id_with_background();
+        let background_id = get_background_id();
+        let library_name = "KEGG_2015";
+        let response = enrich(user_list_id, library_name, Some(&background_id)).unwrap();
         assert!(response.0.contains_key(library_name));
     }
 }
