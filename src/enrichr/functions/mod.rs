46869618f4e4b3aa1f6d91389a872fa02121b744
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-20 14:16:36 -0700

    added enrichr and speedrichr base url as const

--- src/enrichr/functions/mod.rs
+++ src/enrichr/functions/mod.rs
@@ -1,10 +1,15 @@
+mod add_background;
 mod add_list;
 mod enrich;
 mod get_libraries;
 mod shorthand;
 mod view_list;
+pub use add_background::add_background;
 pub use add_list::add_list;
 pub use enrich::enrich;
 pub use get_libraries::get_libraries;
 pub use shorthand::shorthand;
 pub use view_list::view_list;
+
+pub const ENRICHR_URL: &str = "https://maayanlab.cloud/Enrichr";
+pub const SPEEDRICHR_URL: &str = "https://maayanlab.cloud/speedrichr";
