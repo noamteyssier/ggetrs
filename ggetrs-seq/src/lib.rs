0bdf21fc8939fab4762bc168ecf6c2776106242e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-01 15:58:58 -0700

    dep: conditional python compilation

--- ggetrs-seq/src/lib.rs
+++ ggetrs-seq/src/lib.rs
@@ -1,9 +1,13 @@
 mod cli;
 mod functions;
-mod python;
 mod types;
+
+#[cfg(feature = "python")]
+mod python;
 
 pub use cli::launch_seq;
 pub use functions::sequence;
-pub use python::python_seq;
 pub use types::{ResultSeq, ResultSeqContainer};
+
+#[cfg(feature = "python")]
+pub use python::python_seq;
