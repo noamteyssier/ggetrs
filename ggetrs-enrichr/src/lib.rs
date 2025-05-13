a3076aee8d8a46b67921aea77691396a2999b06e
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-13 09:32:13 -0700

    refactor: rename cli to launch

--- ggetrs-enrichr/src/lib.rs
+++ ggetrs-enrichr/src/lib.rs
@@ -4,15 +4,15 @@ pub mod functions;
 /// Module for handling result structs and containers
 pub mod types;
 
-/// submodule for command line usage
-mod cli;
+/// submodule for launching functions
+mod launch;
 
 /// submodule for python usage
 #[cfg(feature = "python")]
 mod python;
 
-pub use cli::{launch_enrichr, launch_enrichr_list};
 pub use functions::{add_list, enrich, get_libraries};
+pub use launch::{launch_enrichr, launch_enrichr_list};
 
 #[cfg(feature = "python")]
 pub use python::{python_enrichr, python_enrichr_background};
