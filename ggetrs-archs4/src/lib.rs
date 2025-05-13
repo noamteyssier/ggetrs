60cab934b5f729a3388354c6b5ef3be7a99348a4
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-13 09:22:45 -0700

    refactor: rename cli to launch

--- ggetrs-archs4/src/lib.rs
+++ ggetrs-archs4/src/lib.rs
@@ -4,15 +4,15 @@ pub mod functions;
 /// Module for handling result structs and containers
 pub mod types;
 
-/// module for command line interface
-mod cli;
+/// module for launching commands
+mod launch;
 
 /// module for python interface
 #[cfg(feature = "python")]
 mod python;
 
-pub use cli::{launch_archs4_correlation, launch_archs4_tissue};
 pub use functions::{correlation, tissue};
+pub use launch::{launch_archs4_correlation, launch_archs4_tissue};
 pub use types::Species;
 
 #[cfg(feature = "python")]
